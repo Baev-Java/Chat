7eb3497397d215a53264f1533f2eb3f6297b2268
Yaroslav <baevarik127@gmail.com>
2023-02-23 18:34:52 +0300

    first

---
+++ app/src/main/java/com/example/chat/User.java
@@ -0,0 +1,43 @@
+package com.example.chat;
+
+public class User {
+
+    private String username;
+    private String email;
+    private String profilePicture;
+
+    public User(){
+
+    }
+
+
+    public User(String username, String email, String profileName) {
+        this.username = username;
+        this.email = email;
+        this.profilePicture = profileName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+}
